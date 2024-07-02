cd5f4bf5b3086a16bb63dc745681c5bae4c1ccf0
Angelo Fonseca <angeloniemeyer97@gmail.com>
2024-07-02 09:15:37 -0300

    Adiciona bootstrap e pagina Home

--- src/types.ts
+++ src/types.ts
@@ -6,10 +6,10 @@ export interface DataType {
   previousPage: number;
   showingFrom: number;
   showingTo: number;
-  items: Item[];
+  items: InitialItem[];
 }
 
-export interface Item {
+export interface InitialItem {
   id: number;
   tipo: string;
   titulo: string;
@@ -22,4 +22,30 @@ export interface Item {
   produtos_relacionados: string;
   destaque: boolean;
   link: string;
+}
+
+export interface Item {
+  id: number;
+  tipo: string;
+  titulo: string;
+  introducao: string;
+  data_publicacao: string;
+  produto_id: number;
+  produtos: string;
+  editorias: string;
+  imagens: Imagens;
+  produtos_relacionados: string;
+  destaque: boolean;
+  link: string;
+}
+
+interface Imagens {
+  image_intro: string;
+  float_intro: string;
+  image_intro_alt: string;
+  image_intro_caption: string;
+  image_fulltext: string;
+  float_fulltext: string;
+  image_fulltext_alt: string;
+  image_fulltext_caption: string;
 }
