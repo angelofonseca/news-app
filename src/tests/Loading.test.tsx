f2fe8fee3cd92187d79c2b014d6b6cda98c5d5c8
Angelo Fonseca <angeloniemeyer97@gmail.com>
2024-07-05 08:23:19 -0300

    testando mock da api

--- src/tests/Loading.test.tsx
+++ src/tests/Loading.test.tsx
@@ -1,31 +1,29 @@
-import { describe, it, vi } from 'vitest';
-import { screen, waitForElementToBeRemoved } from '@testing-library/react';
-import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils';
-import * as ReactQuery from 'react-query';
-import customRender from '../utils/customRender';
-import '@testing-library/jest-dom';
-import MOCK_PAGE_1_DATA from '../utils/mockFirstPage.json';
-import MOCK_PAGE_2_DATA from '../utils/mockSecondPage.json';
+import { QueryClient, QueryClientProvider, useInfiniteQuery } from 'react-query';
+// import { renderHook } from '@testing-library/react-hooks';
 import App from '../App';
+import { it } from 'vitest';
 
-mockAllIsIntersecting(true);
-// const mockUseInfiniteQueryResult: ReactQuery.UseInfiniteQueryResult<any, any> = {
-//   data: MOCK_PAGE_1_DATA, // Your existing MOCK_DATA fits here as the data property
-//   error: null, // Assuming no error for the mock
-//   fetchNextPage: MOCK_PAGE_2_DATA, // Mock implementation of fetchNextPage
-//   hasNextPage: false, // Depending on your MOCK_DATA, adjust accordingly
-//   isFetching: false, // Assuming not fetching initially
-//   isFetchingNextPage: false, // Assuming not fetching next page initially
-//   status: 'success', // Assuming the mock should represent a successful fetch
-//   // Add other properties as needed based on UseInfiniteQueryResult
-// };
-// vi.spyOn(ReactQuery, 'useInfiniteQuery').mockReturnValue(mockUseInfiniteQueryResult);
-describe('MyComponent', () => {
-  it('', async () => {
-    customRender(<App />);
+const queryClient = new QueryClient();
+const wrapper = () => (
+  <QueryClientProvider client={ queryClient }><App /></QueryClientProvider>
+);
 
-    // await waitForElementToBeRemoved(() => screen.getByTestId('loader'));
+function useFetchData() {
+    return useInfiniteQuery({
+      queryKey: ['fetchData'],
+      queryFn: () => request('/api/data'),
+    })
+  }
 
-    screen.debug();
-  });
-});
+const { result, waitFor } = renderHook(() => useCustomHook(), { wrapper });
+
+await waitFor(() => result.current.isSuccess);
+
+it('', () => {
+    await waitFor(() =>
+        expect(mockFn).toHaveBeenCalledWith('some arg'),
+        )
+        })
+})
+
+expect(result.current.data).toEqual('Hello');
