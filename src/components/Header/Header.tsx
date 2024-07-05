dd48158b58f60e59c7205db8e62b51d041387647
Angelo Fonseca <angeloniemeyer97@gmail.com>
2024-07-05 11:08:40 -0300

    Header inicial

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,27 +4,8 @@ import logoNews from '../../images/news-publishing-svgrepo-com .svg';
 function Header() {
   return (
     <header className="d-flex p-2 mb-5 align-items-center g-4">
-      <div className="collapse" id="navbarToggleExternalContent">
-        <div className="bg-dark p-4">
-          <h5 className="text-white h4">Collapsed content</h5>
-          <span className="text-muted">Toggleable via the navbar brand.</span>
-        </div>
-      </div>
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarToggleExternalContent"
-            aria-controls="navbarToggleExternalContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-        </div>
-      </nav>
+      <img src={ logoNews } alt="" className="logo" />
+      <h1 className="mb-0 flex-grow-1 text-center">YOUR NEWS</h1>
     </header>
   );
 }
