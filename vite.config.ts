a598ff0d221e34b8b38833c070c08d3cf7d05747
angelofonseca <angeloniemeyer97@gmail.com>
2024-09-07 19:07:47 -0300

    Ajuste dos testes instala a library whatwg-fetch para conseguir fazer o spyon no fetch e realizar os testes corretamente

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
     coverage: {
       reporter: ['text', 'json', 'html'],
       include: ['src/**/*'],
